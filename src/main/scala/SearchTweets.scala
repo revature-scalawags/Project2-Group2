509aa4eb3bc5b781ba0d8706d76cfae18357216b
Josh Student <jrubin@berkeley.edu>
2021-01-18 11:46:04 -0800

    Handle pagination on SearchTweets.scala

--- src/main/scala/SearchTweets.scala
+++ src/main/scala/SearchTweets.scala
@@ -16,24 +16,49 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import scala.collection.mutable.ArrayBuffer
 
+import scala.util.parsing.json._
 
 object SearchTweets {
 
   @throws[IOException]
   def main(args: Array[String]): Unit = {
 
-    var bearerToken: String = "Sundoo"
+    var bearerToken: String = "YOUR API KEY HERE"
 
     if (bearerToken != null) {
           Future {
 
-      var response: String = getTweets("BurgerKing", bearerToken)
-      println(response)
+            var response: String = getTweets("BurgerKing", bearerToken)
+            println(response)
+            //deconstruct JSON object to find next_token
+            val json:Option[Any] = JSON.parseFull(response)
+            val map:Map[String,Any] = json.get.asInstanceOf[Map[String, Any]]
+            val meta:Map[String, String] = map.get("meta").get.asInstanceOf[Map[String, String]]
+            //if there is next_token, call retrievePages function to recursively retrieve all pages
+            if (meta.contains("next_token")) {
+              val next_token = meta("next_token")
+              retrievePages(next_token)
+            }
           }
     } else {
       println("There was a problem getting you bearer token. Please make sure you set the BEARER_TOKEN environment variable")
     }
-    Thread.sleep(3000)
+    Thread.sleep(10000)
+  }
+
+  //recursively retrieve all pages from API endpoint, printing them to console
+  def retrievePages(next_token: String): Unit = {
+      var bearerToken: String = "AAAAAAAAAAAAAAAAAAAAAKUnLwEAAAAApWuEw9K8SVH7%2BqNNxVtg36Ryr5o%3DP3sGYZngNVtApjFYqjMC5r3lTg8cF4gGIdCtcjUJAjnd23VpEk"
+      var response: String = getTweets("BurgerKing", bearerToken, next_token)
+      println(response)
+
+      val json: Option[Any] = JSON.parseFull(response)
+      val map: Map[String, Any] = json.get.asInstanceOf[Map[String, Any]]
+      val meta: Map[String, String] = map.get("meta").get.asInstanceOf[Map[String, String]]
+      if (meta.contains("next_token")) {
+        val next_token = meta("next_token")
+        retrievePages(next_token)
+      }
   }
 
   @throws[IOException]
@@ -45,6 +70,28 @@ object SearchTweets {
 
     //to search Tweets based on query
     val uriBuilder = new URIBuilder(s"https://api.twitter.com/2/tweets/search/recent?query=from:${query}")
+
+    val httpGet = new HttpGet(uriBuilder.build)
+    httpGet.setHeader("Authorization", s"Bearer ${bearerToken}")
+
+    val response = httpClient.execute(httpGet)
+    val entity = response.getEntity()
+    if (entity != null) {
+      tweetResponse = EntityUtils.toString(entity, "UTF-8")
+    }
+    return tweetResponse
+  }
+
+  //overloaded getTweets with next_token argument
+  @throws[IOException]
+  def getTweets(query: String, bearerToken: String, next_token: String): String ={
+    var tweetResponse: String = ""
+    val httpClient = HttpClients.custom.setDefaultRequestConfig(
+      RequestConfig.custom.setCookieSpec(CookieSpecs.STANDARD).build
+    ).build
+
+    //to search Tweets based on query
+    val uriBuilder = new URIBuilder(s"https://api.twitter.com/2/tweets/search/recent?query=from:${query}&next_token=${next_token}")
 
     val httpGet = new HttpGet(uriBuilder.build)
     httpGet.setHeader("Authorization", s"Bearer ${bearerToken}")
